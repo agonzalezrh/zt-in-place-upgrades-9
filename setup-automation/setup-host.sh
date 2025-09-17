fbd9d18c4538af5ab05c8975580f0d01509c86f5
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2025-09-17 10:46:10 +0200

    Delete setup-automation/setup-host.sh

--- setup-automation/setup-host.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-subscription-manager repos --disable=*
-subscription-manager repos --enable rhel-8-for-x86_64-baseos-rpms
-subscription-manager repos --enable rhel-8-for-x86_64-appstream-rpms
